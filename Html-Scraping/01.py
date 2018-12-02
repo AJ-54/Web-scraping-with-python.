f4be919d6fe3dd366fe9bd7ed266a8f1ba8f2471
Ayush Jain <ayushjai@iitg.ac.in>
2018-12-03 02:06:22 +0530

    Create 01.py

---
+++ Html-Scraping/01.py
@@ -0,0 +1,23 @@
+from lxml import html
+import requests
+
+#Next we will use requests.
+#get to retrieve the web page with our data, parse it using the html module and save the results in tree
+
+page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
+tree = html.fromstring(page.content)
+
+#(We need to use page.content rather than page.text because html.fromstring implicitly expects bytes as input.)
+
+'''tree now contains the whole HTML file in a nice tree structure which we can go over two different ways: XPath and CSSSelect. 
+In this example, we will focus on the former.
+XPath is a way of locating information in structured documents such as HTML or XML documents.'''
+
+#This will create a list of buyers:
+buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+#This will create a list of prices
+prices = tree.xpath('//span[@class="item-price"]/text()')
+
+print "buyers:",buyers
+print "prices:",prices
+
