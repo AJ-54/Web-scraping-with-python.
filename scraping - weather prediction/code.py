0ed99e4b3919c4d74b7f268a39632833976707e1
Ayush Jain <ayushjai@iitg.ac.in>
2018-12-27 17:17:48 +0530

    Create code.py

---
+++ scraping - weather prediction/code.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+response=requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168#.XAkSNGgzZPY")
+print response.status_code
+ 
+soup=BeautifulSoup(response.content, 'html.parser' )
+
+seven_day=soup.find(id="seven-day-forecast")
+forecast_items = seven_day.find_all(class_="tombstone-container")
+#tonight=forecast_items[2]
+print forecast_items
+
+'period = tonight.find(class_="period-name").get_text()
+short_desc = tonight.find(class_="short-desc").get_text()
+temp = tonight.find(class_="temp-high").get_text()
+
+print(period)
+print(short_desc)
+print(temp)
+
+img = tonight.find("img")
+print img.prettify()
+desc = img['title']
+print(desc)
