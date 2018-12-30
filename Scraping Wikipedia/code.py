c8ae3b69facb1358ccac7a3d66ef652e593b5190
Ayush Jain <ayushjai@iitg.ac.in>
2018-12-31 00:53:54 +0530

    Update code.py

--- Scraping Wikipedia/code.py
+++ Scraping Wikipedia/code.py
@@ -10,12 +10,19 @@ import requests
 from bs4 import BeautifulSoup
 
 response=requests.get("https://en.wikipedia.org/wiki/Demographics_of_Toronto_neighbourhoods")
-print response.status_code
+#print response.status_code   - 200 means the request is responded correctly
 
 
 soup=BeautifulSoup(response.content , 'html.parser')
 tsoup= soup.find_all('div' , class_='mw-body-content')[2].find_all('table')[1]
-row_count=1
+style = xlwt.easyxf('font: bold 1') 
+sheet1.write(0,1,'Name',style)
+sheet1.write(0,2,'Population',style)
+sheet1.write(0,3,'Land Area',style)
+sheet1.write(0,4,'Average Income',style)
+sheet1.write(0,5,'Common Language',style)
+row_count=0
+
 for tr in tsoup.find_all('tr'):
 	row_count+=1
 	col_count=0
