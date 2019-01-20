08dfe573ce40829b47755cea5d064405493ebace
Ayush Jain <ayushjai@iitg.ac.in>
2019-01-20 23:28:57 +0530

    Update 01.py

--- Html-Scraping/01.py
+++ Html-Scraping/01.py
@@ -10,7 +10,7 @@ tree = html.fromstring(page.content)
 In this example, we will focus on the former.
 XPath is a way of locating information in structured documents such as HTML or XML documents.'''
 
-#This will create a list of buyers:
+
 buyers = tree.xpath('//div[@title="buyer-name"]/text()')
 
 prices = tree.xpath('//span[@class="item-price"]/text()')
