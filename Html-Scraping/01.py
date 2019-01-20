860556942f3b5498ea659e7b2f34bd6320838ad1
Ayush Jain <ayushjai@iitg.ac.in>
2019-01-20 23:27:35 +0530

    Update 01.py

--- Html-Scraping/01.py
+++ Html-Scraping/01.py
@@ -1,7 +1,7 @@
 from lxml import html
 import requests
 
-#get to retrieve the web page with our data, parse it using the html module and save the results in tree
+#get to retrieve the web page with our data
 
 page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
 tree = html.fromstring(page.content)
